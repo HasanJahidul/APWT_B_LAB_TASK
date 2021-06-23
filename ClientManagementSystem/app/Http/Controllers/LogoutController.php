6f9f2701d2012c0c4d836ff0c0c1457913e653fc
HasanJahidul <joyaiub1@gmail.com>
2021-06-24 03:53:54 +0600

    M2F1 Implemented

--- ClientManagementSystem/app/Http/Controllers/LogoutController.php
+++ ClientManagementSystem/app/Http/Controllers/LogoutController.php
@@ -7,9 +7,7 @@ use Illuminate\Http\Request;
 class LogoutController extends Controller
 {
     public function logout(Request $req){
-        //M2F1
-
-        //
+       
         $req->session()->flush();
         return redirect('/login');
     }
